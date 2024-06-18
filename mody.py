916d80cecfdceed4bd4d75b6e8783facddbbdaae
Nobito-1 <thomssawa86@gmail.com>
2024-06-18 18:21:14 +0200

    Update mody.py

--- mody.py
+++ mody.py
@@ -4,6 +4,6 @@ import os
 class Mody(object):
     ELHYBA = os.environ.get("ELHYBA", "")
     
-    Owner = os.environ.get("Owner", "7246248054")
+    Owner = os.environ.get("Owner", "7212796702")
     
     
